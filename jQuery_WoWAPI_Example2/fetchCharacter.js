e12d700939ede4614ec9178379721dbf3494c3d3
Choonster <Choonster.2010@gmail.com>
2014-01-23 21:40:53 +1100

    Fix "ReferenceError: data is not defined"

--- jQuery_WoWAPI_Example2/fetchCharacter.js
+++ jQuery_WoWAPI_Example2/fetchCharacter.js
@@ -1,18 +1,43 @@
+// Race and class mappings taken from https://github.com/vishnevskiy/battlenet/blob/master/battlenet/enums.py
+var races = [
+	'Human',
+	'Orc',
+	'Dwarf',
+	'Night Elf',
+	'Undead',
+	'Tauren',
+	'Gnome',
+	'Troll',
+	'Goblin',
+	'Blood Elf',
+	'Draenei',
+	'Worgen',
+	'Pandaren (N)',
+	'Pandaren (A)',
+	'Pandaren (H)',
+]
+
+var classes = [
+	'Warrior',
+	'Paladin',
+	'Hunter',
+	'Rogue',
+	'Priest',
+	'Death Knight',
+	'Shaman',
+	'Mage',
+	'Warlock',
+	'Monk',
+	'Druid',
+]
+
 $(document).ready(function(){
-	// $.ajax({
-		// "url": "http://us.battle.net/api/wow/character/aerie-peak/Eldershield",
-		// "type": "GET",
-		// "data": { fields: "items" },
-		// "dataType": "jsonp",
-		// "contentType": "application/json",
-		// "jsonpCallback": "?",
-		// "success": displayCharacterData
-	// })
 	
-	$.getJSON("http://us.battle.net/api/wow/character/aerie-peak/Eldershield?jsonp=?", { fields: "items" }, function(characterData){
+	$.getJSON("http://us.battle.net/api/wow/character/aerie-peak/Eldershield?jsonp=?", { fields: "items" }, function(data){
 		var text = data.name + " is a level " + data.level + " " + (data.gender ? "female" : "male") + " " + races[data.race - 1] +
 			" " + classes[data.class - 1] + " on the realm " + data.realm + "."
 			
 		$("#content").text(text)
 	})
+	
 })
